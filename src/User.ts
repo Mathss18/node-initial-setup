5b1e4305bbf48b71f785e4e84691494a168ce5b1
unknown <theus.7@hotmail.com>
2022-06-09 21:49:37 -0300

    cleaning the project

--- src/User.ts
+++
@@ -1,9 +0,0 @@
-class User{
-  constructor(private name:string, private age:number){}
-
-  speak():string{
-    return `My name is ${this.name} and I am ${this.age} years old`;
-  }
-}
-
-export default User;
--- tests/test.spec.ts
+++
@@ -1,9 +0,0 @@
-import User from "../src/User";
-
-describe("User test", () => {
-  it("should make the user speak", () => {
-    const user = new User("John", 30);
-    const result = user.speak();
-    expect(result).toBe("My name is John and I am 30 years old");
-  });
-});
